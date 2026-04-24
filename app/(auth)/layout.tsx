05ba8b3af9501c2eb96a0af06fd75273a35167dc
Zakaria Sumon <zsumonn@gmail.com>
2026-04-24 23:09:27 +0600

    add suport button

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import DashboardLayoutShell from "@/components/auth/Layout";
+import SupportFloatingButton from "@/components/SupportFloatingButton";
 import { useLoadUserQuery } from "@/redux/features/auth/authApi";
 
 export default function DashboardLayout({
@@ -10,5 +11,12 @@ export default function DashboardLayout({
 }) {
   const { data, isLoading, isError } = useLoadUserQuery();
 
-  return <DashboardLayoutShell>{children}</DashboardLayoutShell>;
+  return (
+    <DashboardLayoutShell>
+      <div>
+        {children}
+        <SupportFloatingButton />
+      </div>
+    </DashboardLayoutShell>
+  );
 }
